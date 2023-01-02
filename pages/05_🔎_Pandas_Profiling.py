b68fe8e8c8ef42ce4f91d0ed0a3a163b636e4bad
jayvannelli <jaymvannelli@gmail.com>
2023-01-02 08:26:13 -0500

    Remove ML model training for now.

--- pages/05_🔎_Pandas_Profiling.py
+++ pages/05_🔎_Pandas_Profiling.py
@@ -4,6 +4,7 @@ import pandas_profiling
 from src.data import get_adidas_sales_df
 from streamlit_pandas_profiling import st_profile_report
 
+
 def main():
     st.title("Adidas Sales Profile Report")
 
